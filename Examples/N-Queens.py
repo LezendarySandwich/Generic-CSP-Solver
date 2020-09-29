8cee0e4f4d95266836d710fdc3666cc200830383
Sanskar <mani.1@iitj.ac.in>
2020-09-29 19:01:58 +0530

    Base structure added

---
+++ Examples/N-Queens.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(0, './../')
+from CSP_Solver.CSP import CSP
+
+# def constraints(obj):
+#     for i in range(1,self.variables+1):
+#         for j in range(i+1,self.variables+1):
+#             obj.add(i,j,'value[i] != value[j]','i','j')
+#             obj.add(i,j,'abs(value[j] - value[i]) != j - i','i','j')
+
+# n = 5
+# a = CSP(n)
+# dom = []
+# for i in range (n):
+#     dom.append(i + 1)
+# a.Domain(dom)
+# constraints(a)
+# a.solve(1)
+
+def constraints(self):
+    for i in range(1,self.variables+1):
+        for j in range(i+1,self.variables+1):
+            self.add(i,j,'value[i] != value[j]','i','j')
+            self.add(i,j,'abs(value[j] - value[i]) != j - i','i','j')
+
+CSP.constraints = constraints
+n = 10
+a = CSP(n)
+dom = []
+for i in range (n):
+    dom.append(i + 1)
+a.Domain(dom)
+a.constraints()
+a.solve(1)
